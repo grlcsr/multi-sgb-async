2b20e2a721ac0b0c61d898793488bbbe4790ba12
crtcsr <sirpsyduck69@gmail.com>
2025-01-23 19:21:25 +0100

    Added graceful shutdown and timeout + management of disconnected and connected boards

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ pub mod raplibs;
 pub mod streamer;
 
 use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-    time::Duration,
+    collections::{HashMap, HashSet},
+    time::{Duration, Instant},
 };
 
 use raplibs::{ftdi_wrapper::list_devices, settings::RunSettings, RapLibErrors};
@@ -13,17 +12,15 @@ use streamer::{
     SingleGeneratorBoardFSM,
 };
 use tokio::{
-    io::AsyncWriteExt,
-    net::{TcpListener, TcpStream},
     runtime::Runtime,
     select, signal,
     sync::mpsc,
-    task::JoinHandle,
+    task::{JoinError, JoinHandle},
     time::sleep,
 };
-use tokio_util::sync::CancellationToken;
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
 
-const LOCAL_ADDRESS: &str = "127.69.42.0:1412";
+//const LOCAL_ADDRESS: &str = "127.69.42.0:1412";
 
 fn main() {
     if let Err(err) = initialize_settings() {
@@ -38,121 +35,113 @@ fn main() {
 async fn async_main() {
     let (tx, rx) = mpsc::channel::<StreamData>(1000);
     let cancellation_token = CancellationToken::new();
-    let listener = TcpListener::bind(LOCAL_ADDRESS).await.unwrap();
-    let socket_wrapper: Arc<Mutex<Option<TcpStream>>> = Arc::new(Mutex::new(None));
-
-    let socket_listener_handler =
-        start_socket_listener_handler(listener, socket_wrapper.clone(), cancellation_token.clone());
+    
     let signal_handler = start_signal_handler(cancellation_token.clone());
     let message_handler =
-        start_message_handler(rx, socket_wrapper.clone(), cancellation_token.clone());
+        start_message_handler(rx, cancellation_token.clone());
 
+    let mut task_tracker = TaskTracker::new();
     let mut device_list = HashMap::new();
-    manage_devices(&mut device_list, &tx, &cancellation_token).await;
+    manage_devices(&mut task_tracker, &mut device_list, &tx, &cancellation_token).await;
 
     signal_handler.await.ok();
-    socket_listener_handler.await.ok();
     message_handler.await.ok();
+    task_tracker.wait().await;
     println!("Completed Tokio!");
 }
 
 fn start_signal_handler(cancellation_token: CancellationToken) -> JoinHandle<()> {
     tokio::spawn(async move {
-        if signal::ctrl_c().await.is_ok() {
-            cancellation_token.cancel();
-        }
-    })
-}
-
-fn start_socket_listener_handler(
-    listener: TcpListener,
-    socket_wrapper: Arc<Mutex<Option<TcpStream>>>,
-    cancellation_token: CancellationToken,
-) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        loop {
-            select! {
-                connection = listener.accept() => {
-                    match connection {
-                        Ok((socket, addr)) => {
-                            println!("New client: {:?}", addr);
-                            let mut socket_arc = socket_wrapper.lock().unwrap();
-                            match *socket_arc {
-                                Some(_) => println!("Connection already open."),
-                                None => *socket_arc = Some(socket)
-                            }
-                        }
-                        Err(e) => println!("Couldn't get client: {:?}", e),
-                    }
-                }
-                _ = cancellation_token.cancelled() => {
-                    println!("socket_handler terminated.");
-                    break;
-                }
-            }
+        select! {
+            _ = signal::ctrl_c() => cancellation_token.cancel(),
+            _ = cancellation_token.cancelled() => return,
         }
     })
 }
 
 fn start_message_handler(
     mut rx: mpsc::Receiver<StreamData>,
-    socket_wrapper: Arc<Mutex<Option<TcpStream>>>,
     cancellation_token: CancellationToken,
 ) -> JoinHandle<()> {
     tokio::spawn(async move {
         loop {
             select! {
-                _ = cancellation_token.cancelled() => rx.close(),
                 message = rx.recv() => {
                     match message {
-                        Some(data) => println!("GOT = {:?}", data),
+                        Some(data) => {
+                            println!("GOT = {:?}", data);
+                            //let serialized = bincode::serialize(&data).unwrap();
+                            //println!("{:?}", serialized);
+                        }
                         None => break
                     }
                 }
+                _ = cancellation_token.cancelled() => rx.close(),
             }
         }
     })
 }
 
 async fn manage_devices(
-    device_list: &mut HashMap<String, JoinHandle<()>>,
+    task_tracker: &mut TaskTracker,
+    device_list: &mut HashMap<String, JoinHandle<Result<(), JoinError>>>,
     tx: &mpsc::Sender<StreamData>,
     cancellation_token: &CancellationToken,
 ) {
+    let mut timeout_check = Instant::now();
     loop {
-        update_device_list(device_list, tx, cancellation_token).await;
+        update_device_list(task_tracker, device_list, tx, cancellation_token).await;        
         sleep(Duration::from_secs(1)).await;
+
+        if !device_list.is_empty() {
+            timeout_check = Instant::now();
+        } else if timeout_check.elapsed() >= Duration::from_secs(10) {
+            println!("No devices connected for 10 seconds, cancelling...");
+            cancellation_token.cancel();
+        }
+
+        if cancellation_token.is_cancelled() {
+            task_tracker.close();
+            break;
+        }
     }
 }
 
 async fn update_device_list(
-    device_list: &mut HashMap<String, JoinHandle<()>>,
+    task_tracker: &mut TaskTracker,
+    device_list: &mut HashMap<String, JoinHandle<Result<(), JoinError>>>,
     tx: &mpsc::Sender<StreamData>,
     cancellation_token: &CancellationToken,
 ) {
+    // Comparing which devices are connected and not yet initiated
+    // and which ones have been disconnected without proper shutdown
     if let Ok(serial_list) = list_devices() {
-        for serial_number in serial_list {
-            match device_list.get(&serial_number) {
-                Some(handle) => {
-                    if cancellation_token.is_cancelled() {
-                        handle.abort();
-                    }
-                    if handle.is_finished() {
-                        println!("Removed board with serial {}", serial_number);
-                        device_list.remove(&serial_number);
-                    }
+        let connected_devices_set: HashSet<_> = device_list.keys().cloned().collect();
+        let serial_set: HashSet<_> = serial_list.iter().cloned().collect();
+
+        let not_in_serial_set: HashSet<_> = connected_devices_set.difference(&serial_set).cloned().collect();
+        let only_in_serial_set: HashSet<_> = serial_set.difference(&connected_devices_set).cloned().collect();
+
+        for serial_number in only_in_serial_set {
+            println!("Adding new board with serial {}", &serial_number);
+            let handle = task_tracker.spawn(start_device(
+                serial_number.clone(),
+                tx.clone(),
+                cancellation_token.clone(),
+            ));
+            device_list.insert(serial_number, handle);
+        }
+
+        for serial_number in not_in_serial_set {
+            if let Some(handle) = device_list.get(&serial_number) {
+                if cancellation_token.is_cancelled() {
+                    handle.abort();
                 }
-                None if !cancellation_token.is_cancelled() => {
-                    println!("Adding new board with serial {}", &serial_number);
-                    let handle = start_device(
-                        serial_number.clone(),
-                        tx.clone(),
-                        cancellation_token.clone(),
-                    );
-                    device_list.insert(serial_number, handle);
+                if handle.is_finished() {
+                    println!("Removed board with serial {}", serial_number);
+                    device_list.remove(&serial_number);
                 }
-                _ => {}
-            }
+            }            
         }
     }
 }
