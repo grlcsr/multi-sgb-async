78344fc4309430f88b148d9ea18a7392343f969b
crtcsr <cesare.caratozzolo@randompower.eu>
2025-05-04 10:34:15 +0200

    fix address

--- socket_reader/reader.py
+++ socket_reader/reader.py
@@ -43,8 +43,8 @@ def read_from_socket(sock):
             display_stats()
 
 if __name__ == "__main__":
-    HOST = "127.69.42.0"
-    PORT = 1412
+    HOST = "127.0.0.1"
+    PORT = 8080
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         try:
